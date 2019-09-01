a282402cd492cb38338dd18bfa794b420237c864
Gintoki-c <490688266@qq.com>
2019-09-01 18:35:48 +0800

    9-1引入了jq 完成了一个动画

--- src/js/es5/index6.js
+++ src/js/es5/index6.js
@@ -1,7 +1,2 @@
 "use strict";
 
-$(function () {
-  $('.logo').click(function () {
-    alert('hello');
-  });
-});
--- src/js/es6/index6.js
+++ src/js/es6/index6.js
@@ -1,5 +1,9 @@
-$(function(){
-	$('.logo').click(function(){
-		alert('hello')
-	})
-})
+/* $(function(){
+	$('.classify').click(function(){
+		let twonav = $('.two_nav')
+			 twonav.slideToggle(400);
+		let topone = $('.top_one')
+			topone.css({"transform":"rotate(-60deg)"})
+		let toptwo = $('.top_two')
+			toptwo.css({'display':'none'})
+	}) */
